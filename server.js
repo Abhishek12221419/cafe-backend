3a05ad8b89532e9413b9e24d222dd2f37c57f863
Abhishek12221419 <abhi2002upadhyay@gmail.com>
2025-07-10 16:00:46 +0530

    updated

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 import {authenticate, authorize} from "./middlewares/auth.js";
 
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER)
 const dbpass = encodeURIComponent(process.env.DBPASS)
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+// mongoose.connect(`mongodb://localhost:27017/mern-cafe`).then(() => {
 //   app.listen(8000, () => {
 //     console.log("Server started");
 //   });
 // });
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.d3eh4gl.mongodb.net/merncafe?retryWrites=true&w=majority&appName=cluster0`).then(() => {
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.d3eh4gl.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=cluster0`).then(() => {
   app.listen(8000, () => {
     console.log("Server started");
   });
